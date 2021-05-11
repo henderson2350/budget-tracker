a31827592049a497231014b1dc7835720083c922
Clare Henderson <henderson2350@gmail.com>
2021-05-11 14:51:57 -0400

    changed service-workers

--- public/service-workers.js
+++ public/service-workers.js
@@ -46,15 +46,6 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
-  // non GET requests are not cached and requests to other origins are not cached
-  if (
-    event.request.method !== "GET" ||
-    !event.request.url.startsWith(self.location.origin)
-  ) {
-    event.respondWith(fetch(event.request));
-    return;
-  }
-
   // handle runtime GET requests for data from /api routes
   if (event.request.url.includes("/api/")) {
     // make network request and fallback to cache if network request fails (offline)
